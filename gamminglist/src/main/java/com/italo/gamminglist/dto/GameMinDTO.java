e9a10aacf6d30918442979d95f4be9726f5d2f8b
italo <italogithub@gmail.com>
2023-05-12 08:49:23 -0300

    create game min dto

---
+++ gamminglist/src/main/java/com/italo/gamminglist/dto/GameMinDTO.java
@@ -0,0 +1,25 @@
+package com.italo.gamminglist.dto;
+
+
+import com.italo.gamminglist.entities.Game;
+
+public class GameMinDTO {
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameMinDTO(){
+
+    }
+
+    public GameMinDTO(Game entity) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+    }
+
+}
