eeb12c0d7fbfa2fd5b9ec273e5eb6c5af6d04508
italo <italogithub@gmail.com>
2023-05-12 08:51:51 -0300

    update

--- gamminglist/src/main/java/com/italo/gamminglist/dto/GameMinDTO.java
+++ gamminglist/src/main/java/com/italo/gamminglist/dto/GameMinDTO.java
@@ -15,11 +15,27 @@ public class GameMinDTO {
     }
 
     public GameMinDTO(Game entity) {
-        this.id = id;
-        this.title = title;
-        this.year = year;
-        this.imgUrl = imgUrl;
-        this.shortDescription = shortDescription;
+        id = entity.getId();
+        title = entity.getTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public Integer getYear() {
+        return year;
+    }
+    public String getImgUrl() {
+        return imgUrl;
+    }
+    public String getShortDescription() {
+        return shortDescription;
     }
 
 }
