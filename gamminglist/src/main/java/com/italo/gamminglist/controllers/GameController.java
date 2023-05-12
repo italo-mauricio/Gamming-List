72f9ca8082206970297ba7e8c7252c085f4c11c2
italo <italogithub@gmail.com>
2023-05-12 09:16:02 -0300

    update controller

--- gamminglist/src/main/java/com/italo/gamminglist/controllers/GameController.java
+++ gamminglist/src/main/java/com/italo/gamminglist/controllers/GameController.java
@@ -1,4 +1,24 @@
 package com.italo.gamminglist.controllers;
 
+import com.italo.gamminglist.entities.Game;
+import com.italo.gamminglist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
 public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<Game> findAll(){
+        List<Game> result = gameService.findAll();
+        return result;
+    }
 }
