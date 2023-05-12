fbfd0f18cf299c0ead5468bb20ce22645f8dc87c
italo <italogithub@gmail.com>
2023-05-11 21:58:58 -0300

    update

---
+++ gamminglist/src/main/java/com/italo/gamminglist/GamminglistApplication.java
@@ -0,0 +1,13 @@
+package com.italo.gamminglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamminglistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamminglistApplication.class, args);
+	}
+
+}
---
+++ gamminglist/src/test/java/com/italo/gamminglist/GamminglistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.italo.gamminglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamminglistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
