5a4774baa74f1b6f14024f02cf82007b821541ae
italo <italogithub@gmail.com>
2023-05-11 22:16:39 -0300

    add getters and setters

--- gamminglist/src/main/java/com/italo/gamminglist/entities/Game.java
+++ gamminglist/src/main/java/com/italo/gamminglist/entities/Game.java
@@ -16,7 +16,6 @@ public class Game {
 
     public Game(Long id, String title, Integer year, String gender, String platform,
                 String imgUrl, String shortDescription, String longDescription) {
-        super();
         this.id = id;
         this.title = title;
         this.year = year;
@@ -24,6 +23,70 @@ public class Game {
         this.platform = platform;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
     }
 }
