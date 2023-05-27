88958d5c8c06fa82bf71cbe18d3bf7bae0ca2e6f
italo <italogithub@gmail.com>
2023-05-27 15:26:47 -0300

    end point funcionando

--- gamminglist/src/main/java/com/italo/gamminglist/services/GameService.java
+++ gamminglist/src/main/java/com/italo/gamminglist/services/GameService.java
@@ -5,6 +5,7 @@ import com.italo.gamminglist.entities.Game;
 import com.italo.gamminglist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
@@ -12,10 +13,12 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public GameDTO findById(Long id){
         Game result = gameRepository.findById(id).get();
         return new GameDTO(result);
     }
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList(); // teste
