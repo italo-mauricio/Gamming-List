f9c2512bf28178158e6a0d65de7b8923b3b9b690
italo <italogithub@gmail.com>
2023-05-27 15:08:17 -0300

    update

---
+++ gamminglist/src/main/java/com/italo/gamminglist/dto/GameDTO.java
@@ -0,0 +1,15 @@
+package com.italo.gamminglist.dto;
+
+public class GameDTO {
+    private Long id;
+    private String title;
+    private Integer year;
+    private String gender;
+    private String platforms;
+    private double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+
+}
